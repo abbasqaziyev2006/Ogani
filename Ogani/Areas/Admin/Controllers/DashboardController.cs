1822f91f81c1da844862fee3c2cbcf581c4df714
abbasqaziyev2006 <abbasgg@code.edu.az>
2025-08-31 11:30:01 +0400

    hhh

--- Ogani/Areas/Admin/Controllers/DashboardController.cs
+++ Ogani/Areas/Admin/Controllers/DashboardController.cs
@@ -4,6 +4,7 @@ namespace Ogani.Areas.Admin.Controllers
 {
     public class DashboardController : AdminController
     {
+        [Area("Admin")]
         public IActionResult Index()
         {
             return View();
