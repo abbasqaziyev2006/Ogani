298349c13add802302119158074baf389d047410
abbasqaziyev2006 <abbasgg@code.edu.az>
2025-08-31 14:00:29 +0400

    fff

--- Ogani/Areas/Admin/Controllers/DashboardController.cs
+++ Ogani/Areas/Admin/Controllers/DashboardController.cs
@@ -4,7 +4,6 @@ namespace Ogani.Areas.Admin.Controllers
 {
     public class DashboardController : AdminController
     {
-        [Area("Admin")]
         public IActionResult Index()
         {
             return View();
